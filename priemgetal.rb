a59f1b6db89ee6eab3639811f6ffd6732f4d1fbf
StijnVanE <45395343+StijnVanE@users.noreply.github.com>
2020-06-15 12:42:20 +0200

    priemgetal.rb verbetering

--- priemgetal.rb
+++ priemgetal.rb
@@ -1,19 +1,19 @@
 puts "geef een getal:"
 getal=gets.chomp.to_i
 count=0
-if (getal==0)
-	puts "0 is niet een priemgetal"
+if getal==0 || getal==1
+	puts "#{getal} is niet een priemgetal"
 else
-	i=2
+	i=1
 	while(i<getal)
 		if (getal%i==0)
 			count+=1
 		end
 		i+=1
 	end
-end
 	if count>1
 		puts "#{getal} is niet een priemgetal"
 	else
 		puts "#{getal} is een priemgetal"
 	end
+end
